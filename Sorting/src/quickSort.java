2b0e506777672e2a0b9446cd1c78935670121f0a
Hemant Basnet <hemantbasnet61@gmail.com>
2025-03-05 21:43:32 +0545

    Quick Sort

--- Sorting/src/quickSort.java
+++ Sorting/src/quickSort.java
@@ -1,2 +1,38 @@
+
+import java.util.Arrays;
+
 public class quickSort {
+    public static void sorting(int[] arr, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(arr, low, high);
+            sorting(arr, low, pivotIndex - 1);
+            sorting(arr, pivotIndex + 1, high);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, high);
+        return i + 1;
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {8, 4, 7, 3, 6, 2, 5};
+        sorting(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
+    }
 }
